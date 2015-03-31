2e2e90d81dfc2119bd0ee7977ab45972a11d4c45
Andrew <andrewkatz00@gmail.com>
2015-03-30 22:44:52 -0700

    Don't use self because it breaks

--- Classes/RMPaperTrailLogger.m
+++ Classes/RMPaperTrailLogger.m
@@ -37,14 +37,14 @@
 {
     static dispatch_once_t pred = 0;
     static RMPaperTrailLogger *_sharedInstance = nil;
-    
+
     dispatch_once(&pred, ^{
         _sharedInstance = [[self alloc] init];
         RMSyslogFormatter *logFormatter = [[RMSyslogFormatter alloc] init];
         _sharedInstance.logFormatter = logFormatter;
         _sharedInstance.useTLS = YES;
     });
-    
+
     return _sharedInstance;
 }
 
@@ -65,22 +65,22 @@
 {
     if (self.host == nil || self.host.length == 0 || self.port == 0)
         return;
-    
+
     NSString *logMsg = logMessage.message;
     if (logMsg == nil) {
         logMsg = @"";
     }
-    
-    if (self.logFormatter) {
-        logMsg = [self.logFormatter formatLogMessage:logMessage];
+
+    if (_logFormatter) {
+        logMsg = [_logFormatter formatLogMessage:logMessage];
     }
-    
+
     //Check if last character is newLine
     unichar lastChar = [logMsg characterAtIndex:logMsg.length-1];
     if (![[NSCharacterSet newlineCharacterSet] characterIsMember:lastChar]) {
         logMsg = [NSString stringWithFormat:@"%@\n", logMsg];
     }
-    
+
     if (!self.useTcp) {
         [self sendLogOverUdp:logMsg];
     } else {
@@ -92,14 +92,14 @@
 {
     if (message == nil || message.length == 0)
         return;
-    
+
     if (self.udpSocket == nil) {
         GCDAsyncUdpSocket *udpSocket = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
         self.udpSocket = udpSocket;
     }
-    
+
     NSData *logData = [message dataUsingEncoding:NSUTF8StringEncoding];
-    
+
     [self.udpSocket sendData:logData toHost:self.host port:self.port withTimeout:-1 tag:1];
 }
 
@@ -107,7 +107,7 @@
 {
     if (message == nil || message.length == 0)
         return;
-    
+
     @synchronized(self) {
         if (self.tcpSocket == nil) {
             GCDAsyncSocket *tcpSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
@@ -115,7 +115,7 @@
             [self connectTcpSocket];
         }
     }
-    
+
     NSData *logData = [message dataUsingEncoding:NSUTF8StringEncoding];
     [self.tcpSocket writeData:logData withTimeout:-1 tag:1];
 }
@@ -124,14 +124,14 @@
 {
     if (self.host == nil || self.port == 0)
         return;
-    
+
     NSError *error = nil;
     [self.tcpSocket connectToHost:self.host onPort:self.port error:&error];
     if (error != nil) {
         NSLog(@"Error connecting to host: %@", error);
         return;
     }
-    
+
 #if !TARGET_OS_IPHONE
     if (self.useTLS) {
         NSLog(@"Starting TLS");
